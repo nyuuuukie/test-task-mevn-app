2b7483db6e9961e8def262c7ed7043902c4d0aae
mhufflep <mhufflep@student.21-school.ru>
2021-08-25 23:27:55 +0300

    installation script done

--- install.sh
+++ install.sh
@@ -1,18 +1,73 @@
-# check node and npm version
+#!/bin/bash
 
-# install dependencies
+cprint () {
+ 
+    declare -A colors;
+    colors=(\
+        ['red']='\E[0;31m'\
+        ['green']='\E[0;32m'\
+        ['yellow']='\E[0;33m'\
+    );
+ 
+    local defaultMsg="";
+    local defaultColor="";
+ 
+    while [[ $# -gt 1 ]];
+    do
+	    case "$1" in
+	        -c|--color)
+	            color="$2";
+	            shift;
+	        ;;
+	    esac
+    	shift;
+    done
+ 
+    message=${1:-$defaultMsg};
+    color=${color:-$defaultColor};
+ 
+    echo -en "${colors[$color]}";
+    echo -e "$message";
+ 
+    tput sgr0;
+    return;
+}
+
+URL="http://localhost:8080"
+SERVER_DIR="server"
+CLIENT_DIR="client"
+OPEN_CMD="open"
+
+# Check Main Dependencies
+declare -a dependencies=("node" "npm")
+
+for dep in ${dependencies[@]}
+do
+	if ! command -v $dep &> /dev/null
+	then
+    	cprint -c red "Error: $dep is not installed"
+		exit
+	fi
+done
+
+# Install
 npm install
 
-# deploy locally
-cd ./server
-npm run devser
-cd -
+# Start Backend
+npm --prefix $SERVER_DIR run devser
 
-cd ./client
-npm run serve
-cd -
+# Start Frontend
+npm --prefix $CLIENT_DIR run serve
 
-# open via browser
+# Open via default browser
+if [ "$(uname)" = "Linux" ]; then
+	OPEN_CMD="xdg-open"
+fi
 
-
-
+if ! command -v "${OPEN_CMD}" &> /dev/null
+then
+	cprint -c yellow "Warning: cannot run your browser"
+	cprint -c yellow "Please, go to $URL manually"
+else
+	$OPEN_CMD $URL > /dev/null 2>&1 & disown
+fi
